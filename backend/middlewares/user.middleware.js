61ee2b7304c94b04fbc26f6a1807c0b9316cc704
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-26 00:33:04 +0300

    Backend <> Added User Middleware

---
+++ backend/middlewares/user.middleware.js
@@ -0,0 +1,19 @@
+const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+const userMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if(!token) return res.status(401).json({message: "Unauthorized"})
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findOne({email: decoded.email}).lean()
+    req.user = user;
+    next()
+
+  } catch(err) {
+    return res.status(401).json({message: "Unauthorized"})
+  }
+}
+
+module.exports = userMiddleware;
