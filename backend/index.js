a64692155f902ed94a6a57af42ffaa0b22e06e7b
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-25 21:24:51 +0300

    Backend <> Added Listener, Tested Server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
@@ -7,3 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT, (err)=>{
+  if(err) throw err;
+  console.log(`server running on port ${process.env.PORT}`);
+})
