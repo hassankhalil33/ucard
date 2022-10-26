c810cedfecbc4c62027132d1c0c0546b6f49de33
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-26 13:57:31 +0300

    Backend <> Added Get User API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,13 @@
 const User = require("../models/user.model");
 const Card = require("../models/card.model");
 
+const getUser = async (req, res) => {
+  const {email} = req.user;
+  const user = await User.find({email});
 
+  res.json(user);
+}
 
 module.exports = {
-
+  getUser
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,14 @@
 const {Router} = require('express');
-const {} = require('../controllers/user.controller')
+const userMiddleware = require('../middlewares/user.middleware');
 const router = Router();
+const {
+  getUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/user.controller')
 
-
+router.get('/', userMiddleware, getUser);
+// router.put('/', userMiddleware, updateUser);
+// router.delete('/', userMiddleware, deleteUser);
 
 module.exports = router;
