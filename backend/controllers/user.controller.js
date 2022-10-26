d56a094f71e285d22425b38e9575438062c5fcf7
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-26 14:23:06 +0300

    Backend <> Added Update User API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,10 +2,21 @@ const User = require("../models/user.model");
 const Card = require("../models/card.model");
 
 const getUser = async (req, res) => {
-  const {email} = req.user;
-  const user = await User.find({email});
+  const user = req.user;
 
   res.json(user);
+}
+
+const updateUser = async (req, res) => {
+  const {_id: id} = req.user;
+  const {name, location} = req.body;
+
+  await User.findByIdAndUpdate(id, {
+    name: name,
+    location: location
+  });
+  
+  res.json({message: "success"})
 }
 
 module.exports = {
