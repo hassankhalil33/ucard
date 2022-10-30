c1581d39a47482377e32c53109db9a81a13b70e5
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-30 22:51:42 +0200

    Frontend <> Updated Welcome Screen, Linked with App

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,34 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import WelcomeScreen from "./app/screens/WelcomeScreen/WelcomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.lowerView}>
-        <Text style={styles.text}>Create Your Business Card in Seconds!</Text>
-      </View>
-    </View>
+    <WelcomeScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#303841",
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row"
-  },
-
-  text: {
-    color: "#f3f3f3",
-    fontSize: 28,
-  },
-
-  lowerView: {
-    alignSelf: "flex-end",
-    padding: 45,
-  }
-});
---
+++ frontend/app/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function WelcomeScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.lowerView}>
+        <Text style={styles.text}>Create Your Business Card in Seconds!</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#303841",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row"
+  },
+
+  text: {
+    color: "#f3f3f3",
+    fontSize: 28,
+  },
+
+  lowerView: {
+    alignSelf: "flex-end",
+    padding: 45,
+  }
+});
