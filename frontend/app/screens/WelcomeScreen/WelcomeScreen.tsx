885eb7fd0ef0cdc2056a2c73828bf8cf4a8c11d2
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-30 23:06:21 +0200

    Frontend <> Added Welcome Button, Text

--- frontend/app/screens/WelcomeScreen/WelcomeScreen.tsx
+++ frontend/app/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableHighlight } from "react-native";
 
 export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.lowerView}>
-        <Text style={styles.text}>Create Your Business Card in Seconds!</Text>
+        <Text style={styles.textMain}>Create Your Business Card in Seconds!</Text>
+        <TouchableHighlight style={styles.button}>
+          <Text style={styles.textInner}>Welcome</Text>
+        </TouchableHighlight>
+        <Text style={styles.textOther}>
+          You have account?
+          <Button title="sign in" />
+        </Text>
       </View>
-    </View>
+    </View >
   );
 }
 
@@ -22,13 +29,36 @@ const styles = StyleSheet.create({
     flexDirection: "row"
   },
 
-  text: {
+  textMain: {
     color: "#f3f3f3",
     fontSize: 28,
+  },
+
+  textInner: {
+    color: "#f3f3f3",
+    fontSize: 16,
+  },
+
+  textOther: {
+    color: "#f3f3f3",
+    fontSize: 11,
   },
 
   lowerView: {
     alignSelf: "flex-end",
     padding: 45,
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#3A4750",
+    marginTop: 12,
+    marginBottom: 12,
+    width: 160,
+    height: 60,
+    borderWidth: 1,
+    borderColor: "rgba(255, 255, 255, 0.17)",
+    borderRadius: 18,
   }
 });
