b6c5307992613ff4bf4e2aa14f8c545644d5ce22
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-01 21:18:49 +0200

    Frontend <> Updated Login Screen

--- frontend/app/screens/LoginScreen/LoginScreen.tsx
+++ frontend/app/screens/LoginScreen/LoginScreen.tsx
@@ -1,2 +1,54 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import colors from "../../constants/pallete";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+const background = require("../../assets/background.png");
+const logo = require("../../assets/Logo.png");
+
+export default function LoginScreen() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+
+  logo: {
+    position: "absolute",
+    height: 58,
+    width: 125,
+    top: 60,
+    left: 45,
+  },
+
+  background: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  backgroundImage: {
+    height: 1000,
+    width: 450,
+  },
+});
