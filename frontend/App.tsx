a20aefdc7a60320b7ec7baf33ed53c6a06d4c122
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-02 21:23:12 +0200

    Frontend <> Updated App.tsx for Debugging

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import WelcomeStackScreen from "./app/routes/welcomeStack";
+import HomeScreen from "./app/screens/HomeScreen/HomeScreen";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <WelcomeStackScreen />
-    </NavigationContainer>
-  );
+    <HomeScreen />
+  )
 }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <WelcomeStackScreen />
+//     </NavigationContainer>
+//   );
+// }
