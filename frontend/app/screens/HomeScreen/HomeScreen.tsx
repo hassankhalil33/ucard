f39c2a6ae57bb0f24d4988a664be2af6acb03579
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-02 21:21:46 +0200

    Frontend <> Created Functional Component Home Screen

--- frontend/app/screens/HomeScreen/HomeScreen.tsx
+++ frontend/app/screens/HomeScreen/HomeScreen.tsx
@@ -4,3 +4,31 @@ import colors from "../../constants/pallete";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 const background = require("../../assets/background.png");
+
+export default function HomeScreen(props) {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+});
