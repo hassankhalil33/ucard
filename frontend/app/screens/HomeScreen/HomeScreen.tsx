f5b3c904b3b10ba9d82a137104bf3e4929c62759
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-02 21:19:31 +0200

    Frontend <> Imported Components to Home Screen

---
+++ frontend/app/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import colors from "../../constants/pallete";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+const background = require("../../assets/background.png");
