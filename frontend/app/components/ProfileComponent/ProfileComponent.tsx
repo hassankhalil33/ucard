ba75085ac657d60b14daf19b69ad56cfe842d5e1
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-03 00:58:32 +0200

    Frontend <> Stylized Profile Component

--- frontend/app/components/ProfileComponent/ProfileComponent.tsx
+++ frontend/app/components/ProfileComponent/ProfileComponent.tsx
@@ -16,22 +16,35 @@ const ProfileComponent: FC<ProfileComponentProps> = (props) => {
   const { name, profession, photo, timestamp, dark, width } = props;
 
   return (
-    <View>
-      <View>
+    <View style={styles(width).container}>
+      <View style={styles().image}>
         <Image source={photo} />
       </View>
 
-      <View>
-        <View>
+      <View style={{ flexDirection: "row", height: 60 }}>
+        <View style={{ height: 60 }}>
           <Text>{name}</Text>
           <Text>{profession}</Text>
         </View>
-        <View>
+        <View style={{ height: 60 }}>
           <Text>{timestamp}</Text>
         </View>
       </View>
     </View>
   )
 }
+
+const styles = (width = 370) => StyleSheet.create({
+  container: {
+    height: 60,
+    width: width,
+    flexDirection: "row"
+  },
+
+  image: {
+    width: 60,
+    height: 60
+  }
+})
 
 export default ProfileComponent
