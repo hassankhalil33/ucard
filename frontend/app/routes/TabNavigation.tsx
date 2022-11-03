f65e124e0fcf15493d919bd9e81eadaeac453b3f
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-03 12:58:53 +0200

    Frontend <> Updated Tab Navigation Routes

---
+++ frontend/app/routes/TabNavigation.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "../screens/HomeScreen/HomeScreen";
+
+const TabNavigation = createBottomTabNavigator();
+const TabNavigationScreen = () => {
+  return (
+    <TabNavigation.Navigator
+      screenOptions={{
+        headerShown: false
+      }}>
+
+      <TabNavigation.Screen
+        name={"home"}
+        component={HomeScreen}
+      />
+      <TabNavigation.Screen
+        name={"notifications"}
+        component={HomeScreen}
+      />
+      <TabNavigation.Screen
+        name={"contacts"}
+        component={HomeScreen}
+      />
+      <TabNavigation.Screen
+        name={"cards"}
+        component={HomeScreen}
+      />
+
+    </TabNavigation.Navigator>
+  )
+}
+
+export default TabNavigationScreen
