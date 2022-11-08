e992adc833f9f9740c865857955aae546e17708b
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-08 14:06:31 +0200

    Frontend <> Moved Card Component Styles to Seperate File

--- frontend/app/components/CardComponent/CardComponent.tsx
+++ frontend/app/components/CardComponent/CardComponent.tsx
@@ -1,12 +1,9 @@
 import React, { FC } from "react";
-import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
-import colors from "../../constants/pallete";
-import { useFonts } from 'expo-font';
+import { Text, View, Image } from "react-native";
+import { useFonts } from "expo-font";
+import styles from "./styles";
 const cardLogoBlue = require("../../assets/icons/IconWhiteonBlue.png");
 const cardLogoOrange = require("../../assets/icons/IconWhiteonOrange.png");
-
-const vw85 = (Dimensions.get('window').width / 10) * 8.5;
-const vw25 = (Dimensions.get('window').height / 10) * 2.5;
 
 interface CardComponentProps {
   height?: number,
@@ -39,46 +36,5 @@ const CardComponent: FC<CardComponentProps> = (props) => {
     </View>
   )
 }
-
-//ratio 1.65
-const styles = (category = "personal", height = 200, width = 330, normal = true) => StyleSheet.create({
-  container: {
-    padding: 30,
-    height: height,
-    width: width,
-    backgroundColor: category == "personal" ? colors.blue : colors.orange,
-    borderWidth: 1,
-    borderColor: "rgba(255, 255, 255, 0.17)",
-    borderRadius: 20,
-  },
-
-  desc: {
-    fontFamily: "Poppins-Regular",
-    color: colors.white,
-    fontSize: 14
-  },
-
-  prof: {
-    fontFamily: "Poppins-Medium",
-    color: colors.white,
-    fontSize: 14,
-    opacity: 0.5,
-    marginTop: normal ? 75 : "28%"
-  },
-
-  name: {
-    fontFamily: "Poppins-Medium",
-    color: colors.white,
-    fontSize: 22,
-  },
-
-  logo: {
-    position: "absolute",
-    height: 27,
-    width: 40,
-    bottom: 30,
-    right: 35,
-  }
-});
 
 export default CardComponent
---
+++ frontend/app/components/CardComponent/styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+import colors from "../../constants/pallete";
+
+const styles = (category = "personal", height = 200, width = 330, normal = true) => StyleSheet.create({
+  container: {
+    padding: 30,
+    height: height,
+    width: width,
+    backgroundColor: category == "personal" ? colors.blue : colors.orange,
+    borderWidth: 1,
+    borderColor: "rgba(255, 255, 255, 0.17)",
+    borderRadius: 20,
+  },
+
+  desc: {
+    fontFamily: "Poppins-Regular",
+    color: colors.white,
+    fontSize: 14
+  },
+
+  prof: {
+    fontFamily: "Poppins-Medium",
+    color: colors.white,
+    fontSize: 14,
+    opacity: 0.5,
+    marginTop: normal ? 75 : "28%"
+  },
+
+  name: {
+    fontFamily: "Poppins-Medium",
+    color: colors.white,
+    fontSize: 22,
+  },
+
+  logo: {
+    position: "absolute",
+    height: 27,
+    width: 40,
+    bottom: 30,
+    right: 35,
+  }
+});
+
+export default styles
