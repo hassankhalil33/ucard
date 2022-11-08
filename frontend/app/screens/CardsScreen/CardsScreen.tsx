ef4772deeb39a70fafc647a60be4edbadc372e20
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-08 13:55:51 +0200

    Frontend <> Moved Card Screen Stlyes to Seperate File

--- frontend/app/screens/CardsScreen/CardsScreen.tsx
+++ frontend/app/screens/CardsScreen/CardsScreen.tsx
@@ -1,9 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image, Dimensions } from "react-native";
+import { Text, View, TouchableOpacity, Image, Dimensions } from "react-native";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { UserContext } from "../../contexts/UserContext";
-import colors from "../../constants/pallete";
+import styles from "./styles";
 import Carousel from "react-native-reanimated-carousel";
 import CardComponent from "../../components/CardComponent/CardComponent";
 import ModalComponent from "../../components/ModalComponent/ModalComponent";
@@ -97,7 +97,7 @@ export default function CardsScreen() {
 
   const renderItems = ({ item }) => {
     return (
-      <View style={styles.card}>
+      <View>
         <CardComponent
           key={item._id}
           category={item.category}
@@ -154,66 +154,3 @@ export default function CardsScreen() {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    flexDirection: "column"
-  },
-
-  upperHalf: {
-    width: "100%",
-    height: "50%",
-    alignItems: "center",
-  },
-
-  background: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  backgroundImage: {
-    height: 1000,
-    width: 450,
-  },
-
-  header: {
-    fontFamily: "Poppins-Bold",
-    fontSize: 24,
-    color: colors.white,
-    paddingTop: 75
-  },
-
-  addButtonContainer: {
-    position: "absolute",
-    top: "10%",
-    right: 40
-  },
-
-  addButton: {
-    width: 30,
-    height: 30.5,
-  },
-
-  card: {
-
-  },
-
-  subHeader: {
-    alignSelf: "flex-start",
-    fontFamily: "Poppins-Medium",
-    fontSize: 16,
-    lineHeight: 24,
-    color: colors.white,
-    marginLeft: 30,
-    marginTop: "20%",
-  },
-})
---
+++ frontend/app/screens/CardsScreen/styles.ts
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+import colors from "../../constants/pallete";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+
+  upperHalf: {
+    width: "100%",
+    height: "50%",
+    alignItems: "center",
+  },
+
+  background: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  backgroundImage: {
+    height: 1000,
+    width: 450,
+  },
+
+  header: {
+    fontFamily: "Poppins-Bold",
+    fontSize: 24,
+    color: colors.white,
+    paddingTop: 75
+  },
+
+  addButtonContainer: {
+    position: "absolute",
+    top: "10%",
+    right: 40
+  },
+
+  addButton: {
+    width: 30,
+    height: 30.5,
+  },
+
+  subHeader: {
+    alignSelf: "flex-start",
+    fontFamily: "Poppins-Medium",
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.white,
+    marginLeft: 30,
+    marginTop: "20%",
+  },
+})
+
+export default styles
