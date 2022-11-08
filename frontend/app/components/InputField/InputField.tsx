0e87a2344442b99342c21aa5e5091e3905a693ad
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-08 15:04:55 +0200

    Frontend <> Moved Input Field Component StyleSheet to Seperate File

--- frontend/app/components/InputField/InputField.tsx
+++ frontend/app/components/InputField/InputField.tsx
@@ -1,8 +1,7 @@
 import React, { useState, FC, useEffect } from "react";
-import { StyleSheet, TextInput } from "react-native";
-import colors from "../../constants/pallete";
-import { useFonts } from 'expo-font';
-import { useFocusEffect } from "@react-navigation/native";
+import { TextInput } from "react-native";
+import { useFonts } from "expo-font";
+import styles from "./styles";
 
 interface InputFieldProps {
   placeholder: string;
@@ -27,7 +26,7 @@ const InputField: FC<InputFieldProps> = ({ placeholder, value, setValue }) => {
 
   return (
     <TextInput
-      style={styles.field}
+      style={styles("Poppins-Medium").field}
       placeholder={placeholder}
       placeholderTextColor={"#989CA0"}
       value={value}
@@ -35,23 +34,5 @@ const InputField: FC<InputFieldProps> = ({ placeholder, value, setValue }) => {
     />
   )
 }
-
-const styles = StyleSheet.create({
-  field: {
-    justifyContent: "center",
-    alignContent: "center",
-    textAlign: "center",
-    fontSize: 18,
-    fontFamily: "Poppins-Medium",
-    width: "100%",
-    height: 50,
-    backgroundColor: "#FFFFFF",
-    color: colors.primary,
-    borderRadius: 10,
-    lineHeight: 27,
-    marginTop: 5,
-    marginBottom: 5
-  }
-})
 
 export default InputField
