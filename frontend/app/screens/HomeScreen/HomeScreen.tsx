08f288d6e0f462e6c732ce0d73d2b88c950673e8
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-09 23:54:07 +0200

    Frontend <> Added AndroidManifest and  aid_list Files to 'main' Folder

--- frontend/app/screens/HomeScreen/HomeScreen.tsx
+++ frontend/app/screens/HomeScreen/HomeScreen.tsx
@@ -77,7 +77,7 @@ export default function HomeScreen() {
           height={vw60}
           normal={false}
           category={item.category}
-          onHold={() => startSession(item._id)}
+          onHold={() => alert("batata")}
         />
       </View>
     );
