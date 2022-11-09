13bf0dc64afa335577ad3aaddcd60b1f64b7a2db
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-09 15:17:20 +0200

    Frontend <> Created Navigation Switcher

---
+++ frontend/app/routes/NavigationSwitcher.tsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import TabNavigationScreen from "./TabNavigation";
+import WelcomeStackScreen from "./WelcomeStack";
+
+const NavigationSwitcher = () => {
+  const { logged } = useContext(UserContext);
+
+  return (
+    logged ? TabNavigationScreen : WelcomeStackScreen
+  )
+}
+
+export default NavigationSwitcher
