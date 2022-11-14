f89f2800d97ad699b6c81bcd21d4cf27d2b4e6d4
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-14 02:48:46 +0200

    Backend <> Updated Notifications Utility to Send Push Notifications

---
+++ backend/utility/notifications.utility.js
@@ -0,0 +1,29 @@
+const { Expo } = require("expo-server-sdk");
+
+let expo = new Expo({});
+let messages = [];
+for (let pushToken of somePushTokens) {
+  if (!Expo.isExpoPushToken(pushToken)) {
+    console.error(`Push token ${pushToken} is not a valid Expo push token`);
+    continue;
+  }
+
+  messages.push({
+    to: pushToken,
+    sound: 'default',
+    title: 'Ucard',
+    body: 'Hello from NodeJS'
+  })
+}
+
+let chunks = expo.chunkPushNotifications(messages);
+(async () => {
+  for (let chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
