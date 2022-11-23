155f0eb0a9da33a3076cf6c50910dbd954edb13e
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-11-24 01:03:49 +0200

    Backend <> Raised File Size Limit for Purpose of Handling Images

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ require('./config/db.config');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json({limit: "2mb"}));
+app.use(bodyParser.urlencoded({limit: "2mb", extended: true}));
 app.use(express.static("public"));
 
 app.use('/auth', authRoutes);
